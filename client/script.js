5c78173a06978f6ed6402e1fa914c196ea63f243
faizan542 <faizansilat100@gmail.com>
2023-03-17 21:04:55 +0500

    fetch data from the server

--- client/script.js
+++ client/script.js
@@ -42,42 +42,68 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return (
-  `
+    `
     <div class="wrapper ${isAi && 'ai'}">
       <div class="chat">
         <div class="profile">
           <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
         </div>
-          <div calss="message" id=${uniqueId}>${value}</div>
+          <div class="message" id=${uniqueId}>${value}</div>
       </div>
     </div>
   `
   )
 };
 
-const handleSubmit = async (e)=>{
+const handleSubmit = async (e) => {
   e.preventDefault();
   const data = new FormData(form);
 
   // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-  
+
   form.reset();
-  
+
   // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  const messageDiv =document.getElementById(uniqueId);
+  const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  // fetch data from server -> bot's response
+  
+  const response = await fetch('http://localhost:5000', {
+    method: "POST",
+    headers:{
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok){
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData)
+  }else{
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
 
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup',(e)=>{
-  if(e.keyCode === 13){
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
     handleSubmit(e);
   };
 });
